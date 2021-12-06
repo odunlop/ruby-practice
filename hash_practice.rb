ce6e390202d92e7ce4dc325281ecfeccc9106f87
Orla <orladunlop2@gmail.com>
2021-12-06 14:28:27 +0000

    More practice using hashes in Ruby

--- hash_practice.rb
+++ hash_practice.rb
@@ -19,10 +19,47 @@ def task_two
   puts "What do you want to view? Name, Home, Pet, Age or Food?"
   response = gets.chomp.downcase
   p profile_hash[:"#{response}"]
-  
+  puts "Time for only 'S' words!"
   profile_hash.each do | key, value |
     if value.chr.include?("S")
       puts value
     end
   end
+end
+
+def task_three
+  dictionary = {
+    :"bear" => "a creature that fishes in the river for salmon.",
+    :"river" => "a body of water that contains salmon, and sometimes bears.",
+    :"salmon" => "a fish, sometimes in a river, sometimes in a bear, and sometimes in both."
+  }
+  
+  puts "What do you want to look up? Bear, river, or salmon?"
+  response = gets.chomp.downcase
+  return dictionary[:"#{response}"]
+end
+
+def task_four
+  players = [
+  { :name => "Sam", :sport => "tennis" },
+  { :name => "Mary", :sport => "squash" },
+  { :name => "Ed", :sport => "tennis" },
+  { :name => "Mark", :sport => "football" }
+  ]
+  
+  players_by_sport = {}
+
+  players.each do |player|
+    sport = player[:sport]
+    name  = player[:name]
+  
+    if players_by_sport[sport] == nil
+      players_by_sport[sport] = []
+    end
+
+    players_by_sport[sport].push(name)
+    
+  end
+  
+  players_by_sport
 end
